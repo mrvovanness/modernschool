b70f88da0cdd6a84b3a187471e7087fcc0bcff7d
mrvovanness <mrvovanness@gmail.com>
2015-05-19 16:57:37 +0500

    Complete user authorization

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module Modernschool
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Config custom generators
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :erb
+      g.test_framework false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
